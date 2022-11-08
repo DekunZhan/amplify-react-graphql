a055d02241ceb71638d81cdf0074b6d1390d42e6
Zhan <dekun.zhan@clp.com.hk>
2022-11-08 21:47:49 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import FilesUploadComponent from './components/files-upload-component';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+// import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    // legend: {
+    //   position: 'top' as const,
+    // },
+    title: {
+      display: true,
+      text: 'Line Chart',
+    },
+  },
+};
+
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+// export const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: 'Dataset 1',
+//       data: labels,
+//       borderColor: 'rgb(255, 99, 132)',
+//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//     },
+//     {
+//       label: 'Dataset 2',
+//       data: labels,
+//       borderColor: 'rgb(53, 162, 235)',
+//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//     },
+//   ],
+// };
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+export const chartdata = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  // React.useEffect(() => {
+  //   fetch("/api?page=10")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+
+        <Line options={options} data={chartdata} />
+
+      </header>
+    </div>
+    
+  );
+  // return <Line data={chartdata} />;
+}
+
+export default App;
